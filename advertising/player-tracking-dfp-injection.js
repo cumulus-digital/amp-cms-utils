b72a6371978d936d919e0fe75764d4455b502484
Daniel Vena <deaconvena+github@gmail.com>
2015-08-26 08:40:04 -0400

    Added DFP criteria injection from player status

---
+++ advertising/player-tracking-dfp-injection.js
@@ -0,0 +1,98 @@
+/**
+ * Watches for changes to song data from the embedded player
+ * and updates DFP targeting criteria on track change.
+ */
+(function(window, undefined) {
+	window._CMLS = window._CMLS || {};
+	window._CMLS.embedPlayerWatch = {
+		v: '0.1',
+		initialized: false,
+		trackIdCache: null,
+		timer: null,
+		interval: 1000,
+
+		log: function log() {
+			if (typeof console === 'object' && console.log) {
+				console.log('[PLAYERWATCH ' + this.v + ']', [].slice.call(arguments));
+			}
+		},
+
+		/**
+		 * Returns true if given track information differs from cache
+		 * @param  {string}  current Track id string from
+		 *                           tdas.controlls.STATION.STREAM.track-meta
+		 *                           localStorage object.
+		 * @return {Boolean}
+		 */
+		isChanged: function testTrack(current) {
+			if (current) {
+				if (current == this.trackIdCache) {
+					return false;
+				}
+				return true;
+			}
+			return false;
+		},
+
+		getTrackInfo: function getTrackInfo() {
+			return JSON.parse(localStorage.getItem('tdas.controller.' + amp_player_config.station + '.' + amp_player_config.stream_id + '.events.track-meta'));
+		},
+
+		getTrackId: function getTrackId() {
+			var info = this.getTrackInfo();
+			if (info && info.id) {
+				return info.id;
+			}
+			return null;
+		},
+
+		checkCurrent: function checkCurrent() {
+			var current = this.getTrackId();
+			if (this.isChanged(current)) {
+				this.log('Song changed!', current);
+				this.trackIdCache = current;
+				this.setCriteria(this.getTrackInfo());
+			}
+			this.setTimer();
+		},
+
+		setCriteria: function setCriteria(meta) {
+			if (window.googletag && googletag.pubadsReady) {
+				if (meta && meta.data && meta.data.data) {
+					meta = meta.data.data;
+					if (meta.artistName) {
+						googletag.pubads().setTargeting('td-player-artist', meta.artistName);
+					}
+					if (meta.collectionName) {
+						googletag.pubads().setTargeting('td-player-album', meta.collectionName);
+					}
+					if (meta.trackName) {
+						googletag.pubads().setTargeting('td-player-track', meta.trackName);
+					}
+				}
+			}
+		},
+
+		setTimer: function setTimer() {
+			var that = this;
+			this.timer = setTimeout(function() {
+				that.checkCurrent();
+			}, this.interval);
+		},
+
+		init: function init() {
+			if ( ! window.amp_player_config && ! window.amp_player_config.station && ! window.amp_player_config.stream_id) {
+				log('amp_player_config not available.');
+				return false;
+			}
+
+			this.trackIdCache = this.getTrackId();
+			this.setTimer();
+			this.initialized = true;
+			this.log('Initialized! Current track ID:', this.trackIdCache);
+			return this;
+		}
+
+	};
+	window._CMLS.embedPlayerWatch.init();
+}(window));
