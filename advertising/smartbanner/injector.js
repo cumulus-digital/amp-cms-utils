3e3ee87ea46f0d3cf743922858941c00eea55734
Daniel Vena <deaconvena+github@gmail.com>
2015-08-31 14:38:41 -0400

    fix stylesheet url

--- advertising/smartbanner/injector.js
+++ advertising/smartbanner/injector.js
@@ -5,7 +5,7 @@
 		headerSelector: '.wrapper-header',
 		logoSelector: 'figure.logo img',
 
-		styleSheetUrl: 'https://cdn.rawgit.com/cumulus-digital/amp-cms-utils/commits/a0b1e63ff6e90594d73b204dd1e01b1f282cc592/advertising/smartbanner/smartbanner.css',
+		styleSheetUrl: 'https://cdn.rawgit.com/cumulus-digital/amp-cms-utils/a0b1e63ff6e90594d73b204dd1e01b1f282cc592/advertising/smartbanner/smartbanner.css',
 		libraryUrl: 'https://cdn.rawgit.com/cumulus-digital/amp-cms-utils/a0b1e63ff6e90594d73b204dd1e01b1f282cc592/advertising/smartbanner/smartbanner.js'
 	};
 	
