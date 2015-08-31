a0b1e63ff6e90594d73b204dd1e01b1f282cc592
Daniel Vena <deaconvena+github@gmail.com>
2015-08-31 14:28:05 -0400

    building our own smart banner library

---
+++ advertising/smartbanner/injector.js
@@ -0,0 +1,189 @@
+(function($, window, undefined) {
+
+	var settings = {
+		appWidgetSelector: '.free-apps,.mobile-apps',
+		headerSelector: '.wrapper-header',
+		logoSelector: 'figure.logo img',
+
+		styleSheetUrl: 'smartbanner.css',
+		libraryUrl: 'smartbanner.js'
+	};
+	
+	var v = '0.8';
+
+	function log() {
+		if (window._CMLS && window._CMLS.debug && typeof console === 'object' && console.log) {
+			console.log('[SMART APP BANNER INJECTOR ' + v + ']', [].slice.call(arguments));
+		}
+	}
+
+	// Only execute once
+	window._CMLS = window._CMLS || {};
+	if (window._CMLS.smartBannerEnabled2 && window._CMLS.smartBannerEnabled2 === true) {
+		log('Already enabled, skipping.');
+		return;
+	}
+
+	/**
+	 * Locates and returns app store IDs from supplied app widget object
+	 * @param  {object} appWidget jQuery object for app widget
+	 * @return {object}           discovered IDs for ios and android
+	 */
+	function getAppIds(appWidget) {
+		var returnLinks = {};
+		var links = {
+			ios: appWidget.find('a[href*="itunes.apple.com/us/app"]:first'),
+			android: appWidget.find('a[href*="play.google.com/store"]:first')
+		};
+		if (links.ios && links.ios.length > 0) {
+			var iOSId = links.ios.attr('href').match(/id(\d+)/i);
+			if (iOSId && iOSId.length) {
+				returnLinks.ios = iOSId[1];
+			}
+		}
+		if (links.android && links.android.length) {
+			var androidId = links.android.attr('href').match(/id=([0-9a-zA-Z\.]+)/i);
+			if (androidId && androidId.length) {
+				returnLinks.android = androidId[1];
+			}
+		}
+		return returnLinks;
+	}
+
+	/**
+	 * Generated meta tag strings from supplied app ID object
+	 * @param  {object} appIds android and ios app ID strings
+	 * @return {array}         array of meta tag strings
+	 */
+	function createMetaTags(appIds) {
+		log('Generating meta tags');
+		var metaTags = [];
+		function toArray(obj) {
+			return obj === null ? [] : (Array.isArray(obj) ? obj : [obj]);
+		}
+		function generateiOSMeta(id) {
+			return '<meta name="apple-itunes-app" content="app-id=' + id + '">';
+		}
+		function generateAndroidMeta(id) {
+			return '<meta name="google-play-app" content="app-id=' + id + '">';
+		}
+		if (appIds.ios) {
+			metaTags = metaTags.concat(toArray(appIds.ios).map(generateiOSMeta));
+		}
+		if (appIds.android) {
+			metaTags = metaTags.concat(toArray(appIds.android).map(generateAndroidMeta));
+		}
+		return metaTags;
+	}
+
+	/**
+	 * Attempts to craft a nice looking title from the supplied page title,
+	 * stripping extraneous bits.
+	 * @param  {string} title original page title
+	 * @return {string}       normalized title
+	 */
+	function createNiceTitle(title) {
+		var normalizedTitle = title.replace(/(\|\s*)?Cumulus(\|\s*)?/, '').match(/\|\s*([^\|]*)(\|.*)?$/);
+		if (normalizedTitle && normalizedTitle.length) {
+			log('Using normalized title', normalizedTitle);
+			return normalizedTitle[1];
+		}
+		return title;
+	}
+
+	/**
+	 * Generates app icon link strings from the site logo
+	 * @param  {string} logoSelector CSS selector for site logo
+	 * @return {array|null}	         array of link strings or null if no logo is found
+	 */
+	function createIconLinks(logoSelector) {
+		var logoUrl = $(logoSelector).attr('src');
+		if (logoUrl && logoUrl.length) {
+			log('Generating icon link tags');
+			return [
+				'<link rel="apple-touch-icon" href="' + logoUrl + '">',
+				'<link rel="android-touch-icon" href="' + logoUrl + '">'
+			];
+		}
+		return null;
+	}
+
+	/**
+	 * Attempts to get the background color of the site masthead
+	 * @param  {string} selector CSS selector for site masthead
+	 * @return {string}          CSS color value
+	 */
+	function getHeaderBackgroundColor(selector) {
+		var color = $(selector).css('backgroundColor');
+		if (color && color.length) return color;
+		return 'rgba(0,0,0,0.5)';
+	}
+
+	/**
+	 * Appends a supplied array or string to the doc head
+	 * @param  {object|array|string} arr   elements to append
+	 */
+	function appendToHead(arr) {
+		$('head').append(arr);
+	}
+
+	function injectSmartBanner() {
+		if ( ! window._CMLS.smartBanner) {
+			setTimeout(injectSmartBanner, 300);
+			return;
+		}
+
+		var appWidget = $(settings.appWidgetSelector);
+		if ( ! appWidget || appWidget.length < 1) {
+			log('No app widget found, aborting.');
+			return;
+		}
+
+		log('Injecting');
+
+		var pageTitle = document.title,
+			iconUrl,
+			iconBackground = getHeaderBackgroundColor(settings.headerSelector),
+			gotLinks = false,
+			headAppend = [];
+
+		var metaTags = createMetaTags(getAppIds(appWidget));
+		if ( ! metaTags || ! metaTags.length) {
+			log('No app IDs found, aborting.');
+		}
+		headAppend = headAppend.concat(metaTags);
+
+		if ( ! $('head link[rel="apple-touch-icon"]').length) {
+			headAppend = headAppend.concat(createIconLinks(settings.logoSelector));
+		}
+
+		pageTitle = createNiceTitle(pageTitle);
+
+		// Add styles
+		headAppend.push(
+			'<link rel="stylesheet" href="' + settings.styleSheetUrl + '" type="text/css">'
+		);
+
+		appendToHead(headAppend);
+
+		window._CMLS.smartBanner({
+			title: pageTitle,
+			author: 'Cumulus',
+			button: 'VIEW'
+		});
+
+		log('Injected');
+		window._CMLS.smartBannerEnabled = true;
+
+	}
+
+	$(function() {
+		if ( ! window._CMLS.smartBanner) {
+			log('Loading SmartBanner library.');
+			$.getScript(settings.libraryUrl, injectSmartBanner);
+			return;
+		}
+		injectSmartBanner();
+	});
+
+}(jQuery, window));
---
+++ advertising/smartbanner/smartbanner.js
@@ -0,0 +1,191 @@
+(function(window, undefined) {
+	
+	window._CMLS = window._CMLS || {};
+	window._CMLS.smartBanner = function(options) {
+		var v = 0.1;
+		function log() {
+			if (window._CMLS && window._CMLS.debug && typeof console === 'object' && console.log) {
+				console.log('[SMART APP BANNER ' + v + ']', [].slice.call(arguments));
+			}
+		}
+
+		options = options || {};
+		var defaults = {
+			containerId: 'cmls-sb',
+			icon: {
+				url: null,
+				color: 'transparent'
+			},
+			title: null,
+			author: null,
+			strings: {
+				store: {
+					apple: 'On the App Store',
+					google: 'In Google Play'
+				},
+				price: {
+					apple: 'FREE',
+					google: 'FREE'
+				},
+				button: 'View'
+			},
+			force: false,
+			daysToHide: 30
+		};
+
+		function extend(o1, o2) {
+			var newO = Object.create(o1);
+			Object.keys(o2).map(function(p) {
+				p in newO && (newO[p] = o2[p]);
+			});
+			return newO;
+		}
+		var settings = extend(defaults, options);
+		log('Loaded with settings', settings);
+
+		var bannerTemplate = '' +
+			'<i id="cmls-sb-close">&times;</i>' +
+			'<div id="cmls-sb-icon"></div>' +
+			'<div id="cmls-sb-info">' +
+				'<div id="cmls-sb-title"></div>' +
+				'<div id="cmls-sb-author"></div>' +
+				'<div id="cmls-sb-instore"></div>' +
+			'</div>' +
+			'<a href="#" id="cmls-sb-link">' + settings.strings.button + '</a>';
+
+		// constants
+		var APPLE = 'apple',
+			GOOGLE = 'google',
+			AMAZON = 'amazon';
+
+		// locals
+		var vendor, banner, cache = {
+			htmlPaddingTop: window.document.documentElement.style.paddingTop
+		};
+
+		function getDevice() {
+			var ua = typeof userAgent != 'undefined' ? userAgent : navigator.userAgent;
+			// Strip Facebook's crap
+			// pulled from https://github.com/kaimallea/isMobile
+			var fb = ua.split('[FBAN');
+			if (typeof fb[1] !== 'undefined') {
+				ua = tmp[0];
+			}
+			log('UA', ua);
+			var maps = [
+				{
+					vendor: APPLE,
+					regexp: /iP[honead]+/i
+				},
+				{
+					vendor: GOOGLE,
+					regexp: /(?=.*\bAndroid\b)(?=.*\bMobile\b)/i
+				},
+				{
+					vendor: AMAZON,
+					regexp: /(?=.*\bAndroid\b)(?=.*\b(?:SD4930UR|KFOT|KFTT|KFJWI|KFJWA|KFSOWI|KFTHWI|KFTHWA|KFAPWI|KFAPWA|KFARWI|KFASWI|KFSAWI|KFSAWA)\b)/i
+				}
+			];
+			for (var i = 0; i < maps.length; i++) {
+				if (ua.match(maps[i].regexp)) return maps[i].vendor;
+			}
+			return false;
+		}
+
+		function getMetaID(store) {
+			var meta, content, id;
+			switch (store) {
+				case APPLE:
+					meta = window.document.querySelector('meta[name="apple-itunes-app"]');
+					break;
+				case GOOGLE:
+					meta = window.document.querySelector('meta[name="google-play-app"]');
+					break;
+			}
+			if (meta)
+				content = meta.getAttribute('content');
+				if (content && content.length)
+					id = /app-id([^\s,]+)/i.exec(content);
+					if ( ! id || ! id.length) id = undefined;
+
+			if (id && id.length) return id;
+			return false;
+		}
+
+		function getStoreLink(store, id) {
+			switch(store) {
+				case APPLE:
+					return 'https://itunes.apple.com/en/app/id' + id;
+				case GOOGLE:
+					return 'http://play.google.com/store/apps/details?id=' + id;
+			}
+			return null;
+		}
+
+		function readCookie(key) {
+			return(document.cookie.match('(^|; )'+key+'=([^;]*)')||0)[2]
+		}
+
+		function setCookie(key, value, exp) {
+			var exdate = new date();
+			exdate.setDate(exdate.getDate()+exp);
+			value = encodeURI(value) + ( ! exp ?  '' : '; expires=' + exdate.toUTCString() );
+			document.cookie=key + '=' + value + '; path=/';
+		}
+
+		function closeBanner() {
+			cache.bannerElement.className = cache.bannerElement.replace('cmls-sb-open', '') + ' cmls-sb-closed';
+			window.document.documentElement.style.paddingTop = cache.htmlPaddingTop;
+			//setCookie('smartbanner-closed', 'true', settings.daysToHide);
+		}
+
+		function injectBanner(banner) {
+			window.document.body.appendChild(banner);
+			cache.bannerElement = window.document.getElementById(settings.containerId);
+			cache.bannerHeight = cache.bannerElement.offsetHeight;
+			cache.bannerElement.style.height = 0;
+
+			window.document.documentElement.className = window.document.documentElement.className + ' cmls-sb-injected';
+			cache.htmlPaddingTop = window.document.documentElement.style.paddingTop;
+			window.document.documentElement.style.paddingTop = cache.bannerHeight;
+			cache.bannerElement.className = cache.bannerElement.className + ' cmls-sb-open';
+		}
+
+		vendor = getDevice();
+		log('Detected device', vendor);
+
+		// eject if we're not mobile, in an app, or banner was closed
+		if ( ! vendor || navigator.standalone || readCookie('sb-closed') || readCookie('smartbanner-closed') || readCookie('smartbanner-installed')) {
+			log('Not on mobile, ejecting.');
+			return;
+		}
+
+		log('Generating banner');
+
+		banner = document.createDocumentFragment();
+		var bannerContainer = document.createElement('div');
+			bannerContainer.id = settings.containerId;
+			bannerContainer.className = vendor == APPLE ? 'cmls-sb-apple' : vendor == GOOGLE ? 'cmls-sb-google' : '';
+			bannerContainer.innerHTML = bannerTemplate;
+		banner.appendChild(bannerContainer);
+
+		banner.getElementById('cmls-sb-icon').style.backgroundImage = settings.icon.url;
+		banner.getElementById('cmls-sb-icon').style.backgroundColor = settings.icon.color;
+		banner.getElementById('cmls-sb-title').innerHTML = settings.title;
+		banner.getElementById('cmls-sb-author').innerHTML = settings.author;
+		banner.getElementById('cmls-sb-instore').innerHTML = settings.strings.price[vendor] + ' &ndash; ' + settings.strings.store[vendor];
+		banner.getElementById('cmls-sb-link').href = getStoreLink(vendor);
+
+		var closeButton = banner.getElementById('cmls-sb-close');
+		closeButton.addEventListener('click', closeBanner, false);
+		closeButton.addEventListener('touchend', closeBanner, false);
+
+		var linkButton = banner.getElementById('cmls-sb-link');
+		linkButton.addEventListener('click', closeBanner, false);
+		linkButton.addEventListener('touchend', closeBanner, false);
+
+		injectBanner(banner.firstChild);
+		log('Injected!');
+	};
+
+}(window));
