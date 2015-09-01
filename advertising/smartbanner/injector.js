c656087097e00d67569815be490c117d734e6f1e
Daniel Vena <deaconvena+github@gmail.com>
2015-08-31 23:34:49 -0400

    update library url

--- advertising/smartbanner/injector.js
+++ advertising/smartbanner/injector.js
@@ -10,7 +10,7 @@
 			logoSelector: 'figure.logo img',
 
 			styleSheetUrl: 'https://cdn.rawgit.com/cumulus-digital/amp-cms-utils/164c63363f3bd28e12419bfd8ef4f6ecac384c15/advertising/smartbanner/smartbanner.css',
-			libraryUrl: 'https://cdn.rawgit.com/cumulus-digital/amp-cms-utils/ea7a4bcea9cadba9393c2a0a90454793b9f98d74/advertising/smartbanner/smartbanner.js'
+			libraryUrl: 'https://cdn.rawgit.com/cumulus-digital/amp-cms-utils/5ae22979e75c8b62a50daa3fc0274e3ca6975423/advertising/smartbanner/smartbanner.js'
 		};
 
 	function log() {
