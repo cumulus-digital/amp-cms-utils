4ddf3bdb2d0b443c2906d6a7f77e59ef13eb734d
Daniel Vena <deaconvena+github@gmail.com>
2015-09-08 12:48:43 -0400

    update iframe-resizer to 3.2.0, change height calculation method, add corp global debug

--- contest-tools/iframeResizer.js
+++ contest-tools/iframeResizer.js
@@ -5,17 +5,20 @@
 			setTimeout(resizeIframe, 100);
 			return;
 		}
+		var isOldIE = (navigator.userAgent.indexOf("MSIE") !== -1);
+		window._CMLS = window._CMLS || {};
+		window._CMLS.debug = window._CMLS.debug || false;
 		var iframe = $('.entry-content iframe');
 		iframe.iFrameResize({
-			log: false,
+			log: window._CMLS.debug,
 			checkOrigin: false,
 			enablePublicMethods: true,
-			heightCalculationMethod: 'taggedElement'
+			heightCalculationMethod: isOldIE ? 'max' : 'lowestElement'
 		});
 	}
 	$(function() {
 		if ( ! $().iFrameResize) {
-			$.getScript('https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.1.1/iframeResizer.min.js', resizeIframe);
+			$.getScript('https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.2.0/iframeResizer.min.js', resizeIframe);
 			return;
 		}
 		resizeIframe();
