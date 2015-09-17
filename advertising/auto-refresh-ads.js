ad55b9ce93972eb4d9c27dcf51762d85b7416893
Daniel Vena <deaconvena+github@gmail.com>
2015-09-17 00:47:51 -0400

    added auto-refresh-ads library

---
+++ advertising/auto-refresh-ads.js
@@ -0,0 +1,54 @@
+/**
+ * Reload ads every 6 minutes
+ */
+(function(window, undefined) {
+	var nameSpace = 'AutoRefreshAds',
+		version = '0.1',
+
+		// Time before refreshing ads in minutes
+		timeout = 6;
+
+	window._CMLS = window._CMLS || {};
+	if (window._CMLS[nameSpace]) return;
+
+	function log() {
+		if (window._CMLS && window._CMLS.debug && typeof console === 'object' && console.log) {
+			var ts = (new Date());
+			ts = ts.toISOString() ? ts.toISOString() : ts.toUTCString();
+			console.log('%c[AUTO REFRESH ADS ' + version + ']', 'background: #557b9e; color: #FFF', ts, [].slice.call(arguments));
+		}
+	}
+
+	window._CMLS[nameSpace] = {
+		timer: null
+	};
+
+	// Restart timer when ads refresh
+	window.addEventListener('td-player.playing', function() {
+		window.googletag = window.googletag || {};
+		window.googletag.cmd = window.googletag.cmd || [];
+		window.googletag.cmd.push(function() {
+			googletag.pubads().addEventListener('slotRenderEnded', function(e) {
+				if (window._CMLS[nameSpace] && window._CMLS[nameSpace].timer) return;
+				log('Starting timer.');
+				window._CMLS[nameSpace].timer = setTimeout(function() {
+					if (googletag && googletag.pubads) {
+						log('Firing refresh.');
+						googletag.pubads().refresh();
+						window._CMLS[nameSpace].timer = null;
+					}
+				}, timeout * 60000);
+			});
+			googletag.pubads().refresh();
+		});
+	}, false);
+
+	// Clear timer when history changes
+	window.addEventListener('statechange', function() {
+		if (window._CMLS && window._CMLS[nameSpace] && window._CMLS[nameSpace].timer) {
+			clearTimeout(window._CMLS[nameSpace].timer);
+			window._CMLS[nameSpace].timer = null;
+		}
+	}, false);
+
+}(window));
