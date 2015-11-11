7b9f0f05b0fc569e496d33ec7907c54df40df28f
Daniel Vena <deaconvena+github@gmail.com>
2015-11-11 12:22:01 -0500

    added logging to common whichPlayer function

--- src/common.js
+++ src/common.js
@@ -178,13 +178,17 @@
 			position: null,
 		};
 		if (window.tgmp) {
+			window._CMLS.logger('COMMON', 'Found TuneGenie player.');
 			discovered.type = window._CMLS.const.PLAYER_TUNEGENIE;
 			if (window.tgmp.options.position && window.tgmp.options.position.toLowerCase() === 'bottom') {
+				window._CMLS.logger('COMMON', 'TuneGenie player is on the bottom.');
 				discovered.position = window._CMLS.const.PLAYER_POSITION_BOTTOM;
 			} else if (window.tgmp.options.position && window.tgmp.options.position.toLowerCase() === 'top') {
+				window._CMLS.logger('COMMON', 'TuneGenie player is on the bottom.');
 				discovered.position = window._CMLS.const.PLAYER_POSITION_TOP;
 			}
 		} else if (window.TDPW) {
+			window._CMLS.logger('COMMON', 'Found Triton player, assuming it\'s on top.');
 			discovered.type = window._CMLS.const.PLAYER_TRITON;
 			discovered.position = window._CMLS.const.PLAYER_POSITION_TOP;
 		}
