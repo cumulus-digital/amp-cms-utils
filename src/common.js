dc508b4df35d7652ffb213783c16bb4529e9b3d5
Daniel Vena <deaconvena+github@gmail.com>
2015-11-11 16:28:17 -0500

    giving a group another try

--- src/common.js
+++ src/common.js
@@ -53,9 +53,13 @@
 		var ts = (new Date());
 		ts = ts.toISOString() ? ts.toISOString() : ts.toUTCString();
 
-		message = ['%c[' + name + ']', 'background: #' + background + '; color: #' + complement].concat(message, ts);
-		
-		console.log.apply(console, message);
+		var header = ['%c[' + name + ']', 'background: #' + background + '; color: #' + complement];
+
+		header.push.apply(header, message);
+
+		console.groupCollapsed.apply(console, header);
+		console.log('sub item test');
+		console.groupEnd();
 	};
 
 	/**
