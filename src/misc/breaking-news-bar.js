a37e808a2512b09f007c6c0de41dc5c5fffd5868
Daniel Vena <deaconvena+github@gmail.com>
2015-12-08 12:13:30 -0500

    Added logging, better basic usage setup

--- src/misc/breaking-news-bar.js
+++ src/misc/breaking-news-bar.js
@@ -5,7 +5,7 @@
  * SIMPLE USAGE:
  * <script>
  * parent._CMLSBreakingNews = parent._CMLSBreakingNews || [];
- * parent._CMLSBreakingNews.push('Text to display in bar', 'http://example.com');
+ * parent._CMLSBreakingNews.push(['Text to display in bar', 'http://example.com']);
  * </script>
  *
  * ADVANCED USAGE:
@@ -13,60 +13,99 @@
  * parent._CMLSBreakingNews = parent._CMLSBreakingNews || [];
  * parent._CMLSBreakingNews.push({
  * 	position: 'below',
+ * 	background: '#349',
  * 	beforeText: 'Not So Breaking News:',
  * 	text: 'This is a more advanced usage which allows more options. It <a href="http://example.com" target="_blank">supports HTML</a>.'
  * });
  * </script>
  */
 (function($, window, undefined) {
+
+	var scriptName = 'BREAKING NEWS BAR',
+		nameSpace = 'BreakingNews',
+		version = '0.7';
+
 	var _CMLS = window._CMLS || {};
-	_CMLS.breakingNews = function(options, link) {
+
+	function log() {
+		_CMLS.logger(scriptName + ' v' + version, arguments);
+	}
+
+	if (!Array.isArray) {
+		Array.isArray = function(arg) {
+		return Object.prototype.toString.call(arg) === '[object Array]';
+		};
+	}
+
+	_CMLS[nameSpace] = function(options, link) {
+
+		log('Called', typeof options, options, link);
+
 		var defaults = {
+			classPrefix: 'cmlsBreakingNews',
+			additionalClass: '',
 			position: 'above',
 			link: null,
 			beforeText: 'Breaking News:',
-			text: null
+			text: null,
+			background: '#900',
+			color: '#fff'
 		}, settings, injectionPoint = '.wrapper-header';
 
-		if (typeof options === "object") {
+		if (typeof options === "object" && ! Array.isArray(options)) {
 			settings = $.extend({}, defaults, options);
-		} else {
+			log('Advanced mode!');
+		} else if (typeof options === "object" && Array.isArray(options) && options.length) {
+			settings = defaults;
+			settings.text = options[0];
+			if (options.length > 1) {
+				link = options[1];
+			}
+			log('Basic mode!');
+		} else if (typeof options !== "object") {
 			settings = defaults;
 			settings.text = options;
+			log('Basic mode!');
+		} else {
+			log('Invalid usage!', options, link);
+			return;
 		}
 
 		if ( ! settings.link && link) {
 			settings.link = link;
 		}
 
-		var stylesheet = '' +
-			'.cmlsBreakingNews-container { display: block; position: relative; float: none; z-index: 10; padding: 1em; color: #fff; background: #900; box-shadow: 0 0 10px rgba(0,0,0,0.4); font-size: 14px; text-decoration: none; outline: 0 }' +
-			'.cmlsBreakingNews-container[href] { cursor: pointer; }' +
-			'.cmlsBreakingNews-container:hover { color: #fff !important; }' +
-			'.cmlsBreakingNews-inner { max-width: 1020px; margin: 0 auto; }' +
-			'.cmlsBreakingNews-before { font-weight: bold; display: inline-block; }' +
-			'.cmlsBreakingNews-inner a { text-decoration: underline !important; color: inherit; }';
+		log('Settings:', settings);
 
-		var template = '<div class="cmlsBreakingNews-container"><div class="cmlsBreakingNews-inner">{{BEFORE}} {{TEXT}}</div></div>';
+		var stylesheet = '' +
+			'.' + settings.classPrefix + '-container { display: block; box-sizing: border-box; position: relative; float: none; overflow: hidden; z-index: 10; padding: 1em; color: ' + settings.color + '; background: ' + settings.background + '; box-shadow: 0 0 10px rgba(0,0,0,0.4); font-size: 14px; line-height: 1.2; text-decoration: none; outline: 0 }' +
+			'.' + settings.classPrefix + '-container[href] { cursor: pointer; }' +
+			'.' + settings.classPrefix + '-container[href]:hover .' + settings.classPrefix + '-before { text-decoration: underline; }' +
+			'.' + settings.classPrefix + '-container:hover { color: ' + settings.color + ' !important; }' +
+			'.' + settings.classPrefix + '-inner { box-sizing: border-box; max-width: 1020px; margin: 0 auto; }' +
+			'.' + settings.classPrefix + '-before { float: left; font-weight: bold; margin-right: .5em; }' +
+			'.' + settings.classPrefix + '-text { overflow: hidden; }' +
+			'.' + settings.classPrefix + '-inner a { text-decoration: underline !important; color: inherit; }';
+
+		var template = '<div class="' + settings.classPrefix + '-container"><div class="' + settings.classPrefix + '-inner">{{BEFORE}}<div class="' + settings.classPrefix + '-text">{{TEXT}}</div></div></div>';
 
 		if (settings.beforeText && settings.beforeText.length) {
-			var before = '<div class="cmlsBreakingNews-before">' + settings.beforeText + '</div>';
+			var before = '<div class="' + settings.classPrefix + '-before">' + settings.beforeText + '</div>';
 			template = template.replace('{{BEFORE}}', before);
 		}
 
 		template = $(template.replace('{{TEXT}}', settings.text));
 
 		if (settings.link && settings.link.length) {
-			var newTemplate = $('<a class="cmlsBreakingNews-container"></a>').append(template.html()).prop({
+			var newTemplate = $('<a class="' + settings.classPrefix + '-container"></a>').append(template.html()).prop({
 				href: settings.link,
 				target: "_blank"
 			});
 			template = newTemplate;
 		}
 
-		if ( ! $('#cmlsBreakingNewsStyles').length) {
-			$('head').prepend("<style id=\"cmlsBreakingNewsStyles\">/* CMLS Breaking News Bar styles */\n" + stylesheet + '</style>');
-		}
+		$('#cmlsBreakingNewsStyles').remove();
+		$('head').append("<style id=\"cmlsBreakingNewsStyles\">/* CMLS Breaking News Bar styles */\n" + stylesheet + '</style>');
 		if (settings.position === 'below') {
 			$(injectionPoint).after(template);
 			return;
@@ -78,16 +117,13 @@
 	var BNInjector = function() {};
 	BNInjector.prototype = [];
 	BNInjector.prototype.push = function() {
-		if (arguments.length && typeof arguments[0] === "object") {
-			_CMLS.breakingNews.apply(null, arguments);
-		} else if (arguments.length > 1) {
-			_CMLS.breakingNews(arguments[0], arguments[1]);
-		}
+		log('Received after-load request.', arguments);
+		_CMLS[nameSpace].apply(null, arguments);
 	};
 
-	if (window._CMLSBreakingNews && window._CMLSBreakingNews.length) {
-		window._CMLSBreakingNews.forEach(_CMLS.breakingNews);
+	if (window['_CMLS' + nameSpace] && window['_CMLS' + nameSpace].length) {
+		window['_CMLS' + nameSpace].forEach(_CMLS[nameSpace]);
 	}
-	window._CMLSBreakingNews = new BNInjector();
+	window['_CMLS' + nameSpace] = new BNInjector();
 
 }(jQuery, window));
