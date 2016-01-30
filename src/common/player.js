bb1932e8916031f94d557ab0f18cafe1cda23f87
Daniel Vena <deaconvena+github@gmail.com>
2016-01-29 19:43:10 -0500

    fix logging calls

--- src/common/player.js
+++ src/common/player.js
@@ -10,29 +10,29 @@
 	 * @return {object} Player type and position
 	 */
 	window._CMLS.whichPlayer = function() {
-		if (window._CMLS.whichPlayerCache) {
-			return window._CMLS.whichPlayerCache;
+		if (window._CMLS.whichPlayer.cache) {
+			return window._CMLS.whichPlayer.cache;
 		}
 		var discovered = {
 			type: null,
 			position: null,
 		};
 		if (window.tgmp) {
-			window._CMLS.logger('COMMON', ['Found TuneGenie player.']);
+			window._CMLS.log('COMMON', ['Found TuneGenie player.']);
 			discovered.type = window._CMLS.const.PLAYER_TUNEGENIE;
 			if (window.tgmp.options.position && window.tgmp.options.position.toLowerCase() === 'bottom') {
-				window._CMLS.logger('COMMON', ['TuneGenie player is on the bottom.']);
+				window._CMLS.log('COMMON', ['TuneGenie player is on the bottom.']);
 				discovered.position = window._CMLS.const.PLAYER_POSITION_BOTTOM;
 			} else if (window.tgmp.options.position && window.tgmp.options.position.toLowerCase() === 'top') {
-				window._CMLS.logger('COMMON', ['TuneGenie player is on the top.']);
+				window._CMLS.log('COMMON', ['TuneGenie player is on the top.']);
 				discovered.position = window._CMLS.const.PLAYER_POSITION_TOP;
 			}
 		} else if (window.TDPW) {
-			window._CMLS.logger('COMMON', ['Found Triton player, assuming it\'s on top.']);
+			window._CMLS.log('COMMON', ['Found Triton player, assuming it\'s on top.']);
 			discovered.type = window._CMLS.const.PLAYER_TRITON;
 			discovered.position = window._CMLS.const.PLAYER_POSITION_TOP;
 		}
-		window._CMLS.whichPlayerCache = discovered;
-		return window._CMLS.whichPlayerCache;
+		window._CMLS.whichPlayer.cache = discovered;
+		return window._CMLS.whichPlayer.cache;
 	};
 }(window));
