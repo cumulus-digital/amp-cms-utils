a4f417c6320a3e203770158ad98e22b7fbe9dcf0
Daniel Vena <deaconvena+github@gmail.com>
2016-02-29 23:00:31 -0500

    fix reference

--- src/misc/auto-reload.js
+++ src/misc/auto-reload.js
@@ -81,8 +81,8 @@
 
 	// Handle any existing requests
 	if (window._CMLS.autoReload && window._CMLS.autoReload.length) {
-		log('Loaded with request.');
-		_CMLS[nameSpace].start(_CMLS.autoReload[_CMLS.autoReload.length]);
+		log('Loaded with request.', window._CMLS.autoReload);
+		_CMLS[nameSpace].start(window._CMLS.autoReload[window._CMLS.autoReload.length]);
 	}
 
 	// Handle future requests
