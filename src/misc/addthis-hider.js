c009d44132ccf93f3d1487dc691a45b05eb37d01
Daniel Vena <deaconvena+github@gmail.com>
2016-04-29 17:43:47 -0400

    add page transition hiding of addthis crap

--- src/misc/addthis-hider.js
+++ src/misc/addthis-hider.js
@@ -1,12 +1,25 @@
-/**
- * Allow hiding of AddThis widgets
- */
 ;(function($, window, undefined) {
 
+	/**
+	 * Allow hiding of AddThis widgets
+	 */
 	$(function(){
 		if (window.NO_ADDTHIS_HERE) {
 			$('.addthis-smartlayers').hide();
 		}
 	});
+
+	/**
+	 * Remove AddThis on page transition
+	 */
+	if (window.self !== window.top) {
+		if (window.top.destroyAddThis){
+			window.top.destroyAddThis();
+		}
+	} else {
+		window.top.destroyAddThis = function() {
+			$(window.top.document.querySelectorAll('.addthis-smartlayers')).hide();
+		};
+	}
 
 }(jQuery, window));
