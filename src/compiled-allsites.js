837185de88ac47458823bb6eef664368d3aed2af
Daniel Vena <deaconvena+github@gmail.com>
2016-04-29 17:45:18 -0400

    remove auto-scroll past leaderboard

--- src/compiled-allsites.js
+++ src/compiled-allsites.js
@@ -8,7 +8,7 @@
 // @codekit-append "advertising/globalize-sgroups.js"
 // @codekit-append "advertising/teadsinjector.js"
 // @codekit-append "advertising/player-tracking-dfp-injection.js"
-// @codekit-append "advertising/auto-scroll-homepage-past-leaderboard.js"
+// codekit-append "advertising/auto-scroll-homepage-past-leaderboard.js"
 // @codekit-append "advertising/auto-refresh-ads.js"
 // @codekit-append "advertising/nav-through-player.js"
 // @codekit-append "advertising/wallpaper-injector.js"
