f53e68357d5fe0eb87a71b5b8561ea8223dda355
Daniel Vena <deaconvena+github@gmail.com>
2016-05-26 14:51:30 -0400

    added logging

--- src/misc/tg-ga-stream-tracking.js
+++ src/misc/tg-ga-stream-tracking.js
@@ -4,17 +4,29 @@
 /* globals TGMP_EVENTS, ga */
 ;(function($, window, undefined){
 
+	var scriptName = 'TG GA STREAM TRACKING',
+		nameSpace = 'tgGaStreamTracker',
+		version = '0.2';
+
 	/**
 	 * Only load if TG player is enabled and Google Analytics is available
 	 */
-	if ( ! window.top.tgmp || ! ga) {
+	if ( ! window.top.tgmp || ! ga || window._CMLS[nameSpace]) {
 		return;
+	}
+
+	window._CMLS[nameSpace] = true;
+
+	function log() {
+		window._CMLS.logger(scriptName + ' v' + version, arguments);
 	}
 
 	window.top.tgmp.addEventListener(TGMP_EVENTS.streamplaying, function(e){
 		if (e === true) {
+			log('Stream started.');
 			ga('send', 'event', 'TuneGenie', 'Stream', 'Start');
 		} else if (e === false) {
+			log('Stream stopped.');
 			ga('send', 'event', 'TuneGenie', 'Stream', 'Stop');
 		}
 	});
