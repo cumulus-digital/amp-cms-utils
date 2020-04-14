cf287a01517d74f28b5a5ae3b0d9980da8692364
Daniel Vena <deaconvena@gmail.com>
2020-04-14 16:24:47 -0400

    push skyscrapers down if breaking news is enabled

--- src/misc/breaking-news-bar.js
+++ src/misc/breaking-news-bar.js
@@ -131,7 +131,7 @@
 		}
 		if (settings.position === 'below') {
 			$(injectionPoint).after(template);
-			$('#cmlsBreakingNewsStyles').append('.takeover-left,.takeover-right,.skyscraper-left,.skyscraper-right { margin-bottom: -' + template.outerHeight() + 'px; }');
+			$('#cmlsBreakingNewsStyles').append('.takeover-left,.takeover-right,.skyscraper-left,.skyscraper-right { margin-top: ' + template.outerHeight() + 'px; }');
 		} else {
 			$(injectionPoint).before(template);
 		}
