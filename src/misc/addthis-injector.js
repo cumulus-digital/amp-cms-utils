7075133cf866897399126218b96afa15e1b6e210
Daniel Vena <deaconvena+github@gmail.com>
2021-07-21 15:41:10 -0400

    use pubid var

--- src/misc/addthis-injector.js
+++ src/misc/addthis-injector.js
@@ -97,7 +97,7 @@
 	scr.onload = function(){
 		//buildLayer();
 	};
-	scr.src = '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55dc79597bae383e';
+	scr.src = '//s7.addthis.com/js/300/addthis_widget.js#pubid=' + addThisPubId;
 	scr.id = nameSpace + '-script';
 	scr.async = true;
 	window.self.document.head.appendChild(scr);
