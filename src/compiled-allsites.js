2edc451cb4af808d455ae3e85ad6ae9c8ebeb00d
Daniel Vena <deaconvena+github@gmail.com>
2022-02-07 12:21:35 -0500

    remove wrapper, move disable to GTM trigger

--- src/compiled-allsites.js
+++ src/compiled-allsites.js
@@ -1,42 +1,36 @@
 /**
  * Compiled script for ALL SITES
  */
-(function($, window, undefined) {
-    if (window.self.location.search.indexOf('cmlsDisable') > -1) {
-        return;
-    }
 
-    // @codekit-append "common.js"
+// @codekit-append "common.js"
 
-    // Advertising interface library
-    // @codekit-append "advertising/taginterface/detector.js"
-    // 
-    // DISABLED codekit-append "advertising/teadsinjector.js"
-    // @codekit-append "advertising/pushdown-vast.js"
-    // DISABLED codekit-append "advertising/liquid-video-injector.js"
-    // DISABLED codekit-append "advertising/auto-scroll-homepage-past-leaderboard.js"
-    // DISABLED codekit-append "advertising/zergnet-injector.js"
-    // DISABLED @codekit-append "advertising/player-tracking-dfp-injection.js"
-    // @codekit-append "advertising/auto-refresh-ads-constant.js"
-    // @codekit-append "advertising/nav-through-player.js"
-    // @codekit-append "advertising/wallpaper-injector.js"
-    // @codekit-append "advertising/player-sponsor-logo.js"
-    // @codekit-append "advertising/after-post-paid-content-injector.js"
-    // @codekit-append "advertising/w7m-adtag-injector.js"
+// Advertising interface library
+// @codekit-append "advertising/taginterface/detector.js"
+// 
+// DISABLED codekit-append "advertising/teadsinjector.js"
+// @codekit-append "advertising/pushdown-vast.js"
+// DISABLED codekit-append "advertising/liquid-video-injector.js"
+// DISABLED codekit-append "advertising/auto-scroll-homepage-past-leaderboard.js"
+// DISABLED codekit-append "advertising/zergnet-injector.js"
+// DISABLED @codekit-append "advertising/player-tracking-dfp-injection.js"
+// @codekit-append "advertising/auto-refresh-ads-constant.js"
+// @codekit-append "advertising/nav-through-player.js"
+// @codekit-append "advertising/wallpaper-injector.js"
+// @codekit-append "advertising/player-sponsor-logo.js"
+// @codekit-append "advertising/after-post-paid-content-injector.js"
+// @codekit-append "advertising/w7m-adtag-injector.js"
 
-    // Misc
-    // DISABLED codekit-append "misc/gtm-promoreel-tracking.js"
-    // DISABLED codekit-append "misc/inform-video-player.js"
-    // @codekit-append "misc/gtm-stream-tracking.js"
-    // @codekit-append "misc/gtm-tabvisibility-tracking.js"
-    // @codekit-append "misc/auto-reload.js"
-    // @codekit-append "misc/addthis-hider.js"
-    // @codekit-append "misc/addthis-injector.js"
-    // @codekit-append "misc/breaking-news-bar.js"
-    // @codekit-append "misc/social-listen-live-link.js"
-    // @codekit-append "misc/tgmp-switchstream.js"
+// Misc
+// DISABLED codekit-append "misc/gtm-promoreel-tracking.js"
+// DISABLED codekit-append "misc/inform-video-player.js"
+// @codekit-append "misc/gtm-stream-tracking.js"
+// @codekit-append "misc/gtm-tabvisibility-tracking.js"
+// @codekit-append "misc/auto-reload.js"
+// @codekit-append "misc/addthis-hider.js"
+// @codekit-append "misc/addthis-injector.js"
+// @codekit-append "misc/breaking-news-bar.js"
+// @codekit-append "misc/social-listen-live-link.js"
+// @codekit-append "misc/tgmp-switchstream.js"
 
-    // Globalize sgroups at the end
-    // @codekit-append "advertising/globalize-sgroups.js"
-
-})(jQuery, window);
+// Globalize sgroups at the end
+// @codekit-append "advertising/globalize-sgroups.js"
